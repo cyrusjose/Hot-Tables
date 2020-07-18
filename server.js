26c29d1ebd06f26122bd5a35123386f8620dee30
kate <kan004@ucr.edu>
2020-07-18 11:15:49 -0700

    added placeholder array for reservations

--- server.js
+++ server.js
@@ -11,6 +11,28 @@ var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//reservation array placeholders
+var characters = [
+  {
+    name: "cyrus",
+    phone: "555-555-5555",
+    email: "cyrus@gmail.com",
+    id: 1,
+  },
+  {
+    name: "kevin",
+    phone: "222-222-2222",
+    email: "kevin@gmail.com",
+    id: 2,
+  },
+  {
+    name: "kate",
+    phone: "333-333-3333",
+    email: "kate@gmail.com",
+    id: 3,
+  }
+];
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
